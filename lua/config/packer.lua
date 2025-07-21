5e312ffe2151b1d01ec0dca401190237448f8648
Danilo Patrial <danmcamposp@gmail.com>
2025-07-21 10:17:43 -0300

    Package manager - packer.nvim

---
+++ lua/config/packer.lua
@@ -0,0 +1,9 @@
+-- Packer.vim from https://github.com/wbthomason/packer.nvim
+
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manager itself
+  use 'wbthomason/packer.nvim'
+
+end)
