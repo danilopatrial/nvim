3141f6c2308e1d061f9228b9360ee2b31701b395
Danilo Patrial <danmcamposp@gmail.com>
2025-07-21 19:28:26 -0300

    Fugitive harpoon treesitter and undotree

--- lua/config/packer.lua
+++ lua/config/packer.lua
@@ -3,32 +3,42 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
 
-  -- Telescope (fuzzy finder)
-  use {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.8',
-    requires = { { 'nvim-lua/plenary.nvim' } }
-  }
+	-- Telescope (fuzzy finder)
+	use {
+		'nvim-telescope/telescope.nvim',
+		tag = '0.1.8',
+		requires = { { 'nvim-lua/plenary.nvim' } }
+	}
 
-  -- Catppuccin theme
-  use {
-    "catppuccin/nvim",
-    as = "catppuccin"
-    -- NOTE: Don't set colorscheme here, do it in after/plugin/colors.lua
-  }
+	-- Catppuccin theme
+	use {
+		"catppuccin/nvim",
+		as = "catppuccin"
+		-- NOTE: Don't set colorscheme here, do it in after/plugin/colors.lua
+	}
 
-  -- Treesitter
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate'
-  }
+	-- Treesitter
+	use {
+		'nvim-treesitter/nvim-treesitter',
+		run = ':TSUpdate'
+	}
+	use 'nvim-treesitter/playground'
 
-  -- Automatically set up configuration after cloning packer.nvim
-  if packer_bootstrap then
-    require("packer").sync()
-  end
+	-- Harpoon
+	use 'theprimeagen/harpoon'
+
+	-- Undo Tree
+	use 'mbbill/undotree'
+
+	-- Fugitive
+	use 'tpope/vim-fugitive'
+
+	-- Automatically set up configuration after cloning packer.nvim
+	if packer_bootstrap then
+		require("packer").sync()
+	end
 end)
 
