f5608c27270996a9dac237fd1abc6b5b1dd7735d
Danilo Patrial <danmcamposp@gmail.com>
2025-07-21 18:59:01 -0300

    Treesitter

--- lua/config/packer.lua
+++ lua/config/packer.lua
@@ -1,20 +1,34 @@
 -- Packer.vim from https://github.com/wbthomason/packer.nvim
-
+-- Ensure packer is loaded
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	-- Packer can manager itself
-	use 'wbthomason/packer.nvim'
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
 
-	use {  -- Telescope finder
-		'nvim-telescope/telescope.nvim', tag = '0.1.8',
-		-- or                          , branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
-	use ({  -- Catppuccin theme
-		"catppuccin/nvim", as = "catppuccin",
-		config = function()
-			vim.cmd("colorscheme catppuccin")
-		end
-	})
+  -- Telescope (fuzzy finder)
+  use {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
+    requires = { { 'nvim-lua/plenary.nvim' } }
+  }
+
+  -- Catppuccin theme
+  use {
+    "catppuccin/nvim",
+    as = "catppuccin"
+    -- NOTE: Don't set colorscheme here, do it in after/plugin/colors.lua
+  }
+
+  -- Treesitter
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'
+  }
+
+  -- Automatically set up configuration after cloning packer.nvim
+  if packer_bootstrap then
+    require("packer").sync()
+  end
 end)
+
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -74,34 +74,19 @@ end
 time([[try_loadstring definition]], false)
 time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
-  catppuccin = {
-    config = { "\27LJ\2\n:\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\27colorscheme catppuccin\bcmd\bvim\0" },
+  ["nvim-treesitter"] = {
     loaded = true,
-    path = "C:\\Users\\Danilo Patrial\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\catppuccin",
-    url = "https://github.com/catppuccin/nvim"
+    path = "C:\\Users\\Danilo Patrial\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\nvim-treesitter",
+    url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
   ["packer.nvim"] = {
     loaded = true,
     path = "C:\\Users\\Danilo Patrial\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
-  },
-  ["plenary.nvim"] = {
-    loaded = true,
-    path = "C:\\Users\\Danilo Patrial\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\plenary.nvim",
-    url = "https://github.com/nvim-lua/plenary.nvim"
-  },
-  ["telescope.nvim"] = {
-    loaded = true,
-    path = "C:\\Users\\Danilo Patrial\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\telescope.nvim",
-    url = "https://github.com/nvim-telescope/telescope.nvim"
   }
 }
 
 time([[Defining packer_plugins]], false)
--- Config for: catppuccin
-time([[Config for catppuccin]], true)
-try_loadstring("\27LJ\2\n:\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\27colorscheme catppuccin\bcmd\bvim\0", "config", "catppuccin")
-time([[Config for catppuccin]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
