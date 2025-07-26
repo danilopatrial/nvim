9a22e5b29b0ee6ad061ce2a8662af5997e7040a4
Danilo Patrial <danmcamposp@gmail.com>
2025-07-26 12:29:33 -0300

    Hover

--- init.lua
+++ init.lua
@@ -5,4 +5,29 @@ vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.o.laststatus = 0
 
+vim.o.updatetime = 300  -- delay in ms
+
+-- Hover in normal mode
+vim.api.nvim_create_autocmd("CursorHold", {
+    callback = function()
+        vim.lsp.buf.hover()
+    end
+})
+
+-- Hover in insert mode
+vim.api.nvim_create_autocmd("CursorHoldI", {
+    callback = function()
+        vim.lsp.buf.hover()
+    end
+})
+
+-- Add rounded border to hover window
+local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+  opts = opts or {}
+  opts.border = "rounded"
+  return orig_util_open_floating_preview(contents, syntax, opts, ...)
+end
+
+
 
