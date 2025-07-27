c7ab55752b517e6d5c9b1b230793fd74f40d87bf
Danilo Patrial <danmcamposp@gmail.com>
2025-07-27 16:53:03 -0300

    remap

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -3,3 +3,8 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)  -- Set (in normal mode - "n") lea
 vim.keymap.set("n", "<leader>s", vim.cmd.vsp)  -- leader + s to split screen
 vim.keymap.set("i", "<C-x>", "<Esc>", { noremap = true, silent = true })  -- ctrl + x to out insert mode
 
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
+
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down" })
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line up" })
