226fb78be1e548d1c81258e8588f89d121ec0b71
Danilo Patrial <danmcamposp@gmail.com>
2025-07-28 08:50:08 -0300

    fix

--- init.lua
+++ init.lua
@@ -22,11 +22,11 @@ vim.api.nvim_create_autocmd("CursorHold", {
 })
 
 -- Hover in insert mode (warn: This might bring problems when writing the file if the hover in on.)
-vim.api.nvim_create_autocmd("CursorHoldI", {
-    callback = function()
-        vim.lsp.buf.hover()
-    end
-})
+--vim.api.nvim_create_autocmd("CursorHoldI", {
+--    callback = function()
+--        vim.lsp.buf.hover()
+--    end
+--})
 
 -- Add rounded border to hover window
 local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
